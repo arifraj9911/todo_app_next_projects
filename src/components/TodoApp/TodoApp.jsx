3c83189fdfbed8f2931dba4a33aa732511e87576
Arif Hossain <arif.raj9911@gmail.com>
2024-08-03 14:19:45 +0600

    todo task added filed added

--- src/components/TodoApp/TodoApp.jsx
+++ src/components/TodoApp/TodoApp.jsx
@@ -1,18 +1,48 @@
-import React from 'react';
+import React from "react";
 import { IoDocumentTextOutline } from "react-icons/io5";
 import { IoMdAddCircleOutline } from "react-icons/io";
 
 const TodoApp = () => {
-    return (
-        <div className='w-1/3 mx-auto'>
-            <h2 className='text-4xl text-center mb-10 font-bold relative'>Simple Todo App</h2>
-            <div className=''>
-                <span className='absolute top-[194px] left-[500px]'><IoDocumentTextOutline className='text-2xl' /></span>
-                <input type="text" name="" className='py-5 w-full px-10 rounded-md  outline-none bg-[#F3F4F6]' placeholder='type your todo...'   id="" />
-                <span className='absolute top-[194px] right-[500px]'><IoMdAddCircleOutline className='text-2xl'/></span>
-            </div>
+  return (
+    <div className="w-2/4 mx-auto ">
+      <h2 className="text-4xl text-center mb-10 font-bold relative">
+        Simple Todo App
+      </h2>
+      <div className="bg-white p-8">
+        <span className="absolute top-[228px] left-[430px]">
+          <IoDocumentTextOutline className="text-2xl" />
+        </span>
+        <input
+          type="text"
+          name=""
+          className="py-5 w-full px-10 rounded-md  outline-none bg-[#F3F4F6]"
+          placeholder="type your todo..."
+          id=""
+        />
+        <span className="absolute top-[228px] right-[430px]">
+          <IoMdAddCircleOutline className="text-2xl" />
+        </span>
+        <hr className="my-6" />
+        <div className="flex items-center">
+          <div className="rounded-full bg-white border-2  w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 border-green-500 focus-within:border-green-500">
+            <input
+              type="checkbox"
+              className="opacity-0 absolute rounded-full"
+              name=""
+              id=""
+            />
+            <svg
+              className=" fill-current w-3 h-3 text-green-500 pointer-events-none"
+              viewBox="0 0 20 20"
+            >
+              <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+            </svg>
+          </div>
+          <span>Exploring JavaScript Language</span>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default TodoApp;
